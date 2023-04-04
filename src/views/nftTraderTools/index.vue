9a4afe9f41b39186e7c736d8c118cc07caa0491e
Avery <1064614313@qq.com>
2023-04-04 21:43:22 +0800

    搭建页面基本框架，引入element-ui

---
+++ src/views/nftTraderTools/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="nftTraderTools">
+    123
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NftTraderTools'
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/otherTools/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="otherTools">
+    <h1>Here is other tools page!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'OtherTools'
+}
+</script>
+
+<style>
+
+</style>
